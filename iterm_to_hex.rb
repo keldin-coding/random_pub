47c14a2404c7c408f4d0de85cec1115c5057a6ba
Jon Anderson <coding@jpanderson.dev>
2024-12-27 21:48:32 -0800

    Create iterm_to_hex.rb

---
+++ iterm_to_hex.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+
+  gem 'nokogiri'
+end
+
+
+PARSINGS = {
+  /^Ansi (\d+) Color$/ => -> (color_num) { "palette = #{color_num}=%s" },
+  /^Background Color$/ => -> (_) { "background = %s" },
+  /^Foreground Color$/ => -> (_) { "foreground = %s"},
+  /^Selection Text Color$/ => -> (_) { "selection-foreground = %s"},
+  /^Selection Color$/ => -> (_) { "selection-background = %s"},
+  /^Cursor Color$/ => -> (_) { "cursor-color = %s" },
+}
+
+color_values = []
+
+doc = Nokogiri::XML(ARGF.read)
+
+root_dict = doc.xpath("./plist/dict")
+
+def color_to_hex_code(color)
+  color = color.to_f
+
+  (color * 255).floor.to_s(16).ljust(2, "0")
+end
+
+root_dict.children.reject(&:text?).each_slice(2) do |key, color_dictionary|
+  pattern, value_proc = PARSINGS.find { |pattern, _| pattern.match?(key.content) }
+
+  next if !pattern
+
+  matches = pattern.match(key.content)
+
+  color_data = {red: nil, blue: nil, green: nil}
+
+  color_dictionary.children.reject(&:text?).each_slice(2) do |key, value|
+    case key.content
+    when "Blue Component" then color_data[:blue] = color_to_hex_code(value.content)
+    when "Red Component" then color_data[:red] = color_to_hex_code(value.content)
+    when "Green Component" then color_data[:green] = color_to_hex_code(value.content)
+    end
+  end
+
+  color_str = "#{color_data[:red]}#{color_data[:green]}#{color_data[:blue]}"
+
+  color_values << (value_proc.call(matches[1]) % color_str)
+end
+
+puts color_values
